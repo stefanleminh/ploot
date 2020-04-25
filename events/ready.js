ba0a66a80632fef6335e3eac53aab0dc9f2b2fda
Stefan Le-Minh <stefanleminh@gmail.com>
2020-04-25 02:07:35 +0200

    Added custom status

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,8 @@
-module.exports = client => {
+module.exports = (client) => {
   // Log that the bot is online.
   client.logger.log(
     `${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`,
     "ready"
   );
+  client.user.setActivity(" Mr. Doofenshmirtz 👀", { type: "WATCHING" });
 };
