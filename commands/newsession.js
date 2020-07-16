37065a18994a52afbd01e601671d84cfa6c7d528
Stefan Le-Minh <stefanleminh@gmail.com>
2020-07-16 20:26:12 +0200

    Make check for channel name case insensitive

--- commands/newsession.js
+++ commands/newsession.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     return;
   }
   args.forEach((channelName) => {
-    const channel = message.guild.channels.cache.find(channel => channel.name.includes(channelName) && channel.type === "voice");
+    const channel = message.guild.channels.cache.find(channel => channel.name.toLowerCase().includes(channelName.toLowerCase()) && channel.type === "voice");
 
     if(channel === undefined) {
       message.channel.send(
@@ -22,5 +22,5 @@ exports.run = (client, message, args) => {
   if(client.voiceChannels.length != 3) {
     message.channel.send("Unable to add channels to start a session! Please try again or check the help command.")
   }
-  message.channel.send("New session has been created! " + client.voiceChannels[0].name + " is the General Lobby. " + client.voiceChannels[1].name + " is the first team's lobby. " + client.voiceChannels[2].name + " is the second team's lobby. " + "You can now add users.")
+  message.channel.send("New session has been created! " + client.voiceChannels[0].name + " is the general/spectator's lobby. " + client.voiceChannels[1].name + " is the first team's lobby. " + client.voiceChannels[2].name + " is the second team's lobby. " + "You can now add users.")
 };
