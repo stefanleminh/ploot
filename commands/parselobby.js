19e75c50a4c1f352cc2a2ee134d02c90bcac0745
Stefan Le-Minh <stefanleminh@gmail.com>
2021-05-27 02:00:38 +0200

    Add description and args to parselobby

--- commands/parselobby.js
+++ commands/parselobby.js
@@ -3,6 +3,8 @@ const functions = require('../modules/functions')
 module.exports = {
   name: 'parselobby',
   aliases: ['pl'],
+  description: 'Adds every user in the lobby to the list of active players',
+  args: '',
   requiresActiveSession: true,
   execute (message, args, client) {
     message.guild.channels.cache.get(client.config.lobby).members.forEach((member) => {
