39513fc75314aa420fa7703f715e2d4f66e7646d
Stefan Le-Minh <stefanleminh@gmail.com>
2021-06-13 03:29:21 +0200

    Add bot recognition in voicestateupdate

--- events/voicestateupdate.js
+++ events/voicestateupdate.js
@@ -14,13 +14,21 @@ module.exports = {
     if (oldUserChannel === null && newState.channelID === client.config.lobby) {
       logger.info(`User ${newState.member.user.username} has joined the lobby`)
       if (client.currentPlayers.includes(newState.member.user)) {
-        logger.debug('Participant ' + newState.member.user.username + 'already exists in lobby!')
+        logger.debug('Participant ' + newState.member.user.username + ' already exists in lobby!')
+        return
+      }
+      if (newState.member.user.bot) {
+        logger.debug('Participant ' + newState.member.user.username + ' is a bot and will not be added.')
         return
       }
       client.currentPlayers.push(newState.member.user)
       client.channels.cache.get(client.config.botChannel).send('Added player <@' + newState.member.id + '> as a player.')
       logger.debug('Added participant ' + newState.member.user.username + ' to lobby')
     } else if (newUserChannel === null && oldState.channelID === client.config.lobby) {
+      if (oldState.member.user.bot) {
+        logger.debug('Participant ' + newState.member.user.username + ' is a bot and will be ignored.')
+        return
+      }
       logger.info(`User ${oldState.member.user.username} has left the lobby`)
       client.currentPlayers = client.currentPlayers.filter((player) => player !== oldState.member.user)
       client.currentSpectators = client.currentSpectators.filter((spectator) => spectator !== oldState.member.user)
