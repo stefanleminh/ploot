d15fb5d215349fa754bcc585be33fc90dd2935c7
Stefan Le-Minh <stefanleminh@gmail.com>
2021-08-19 12:25:14 +0200

    Add check on whether participant is currently a spectator

--- commands/parselobby.js
+++ commands/parselobby.js
@@ -8,7 +8,9 @@ module.exports = {
   requiresActiveSession: true,
   execute (message, args, client) {
     message.guild.channels.cache.get(client.config.lobby).members.forEach((member) => {
-      functions.addParticipant(member.user, message, client.currentPlayers, 'players')
+      if (!client.currentSpectators.includes(member.user)) {
+        functions.addParticipant(member.user, message, client.currentPlayers, 'players')
+      }
     })
   }
 }
