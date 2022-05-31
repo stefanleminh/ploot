f3a8e9b8f15c8b3f55745ddd3b3a05e5ff706a65
Stefan <stefanleminh@gmail.com>
2022-05-31 17:07:33 +0200

    implement switchmode

--- commands/switchmode.js
+++ commands/switchmode.js
@@ -6,7 +6,7 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('switchmode')
     .setDescription(
-      'Switches the player form active player to spectator or vise versa.'
+      'Switches the player from active player to spectator or vise versa.'
     )
     .addUserOption(option =>
       option
@@ -18,10 +18,21 @@ module.exports = {
   requiresActiveSession: true,
   async execute (interaction, client) {
     const userParameter = interaction.options.getUser('user')
-    // TODO: Only get members in VC
-    const guildUser = await interaction.guild.members.fetch(userParameter.id)
+    const guildUser = await interaction.guild.channels.cache
+      .get(client.config.lobby)
+      .members.get(userParameter.id)
 
-    const isSpectator = Array.from(guildUser.roles.cache.keys()).includes(
+    if (!guildUser) {
+      logger.info(
+        `User ${userParameter.username} not found as an active player or spectator`
+      )
+      await interaction.reply(
+        `Participant <@${userParameter.id}> not found as active player or spectator.`
+      )
+      return
+    }
+
+    const isSpectator = [...guildUser.roles.cache.keys()].includes(
       client.spectatorRoleId
     )
     if (!isSpectator) {
@@ -29,19 +40,12 @@ module.exports = {
         logger.info(`User ${userParameter.username} is now a spectator`)
         interaction.reply(`<@${userParameter.id}> is now spectator.`)
       })
-    } else if (isSpectator) {
+    } else {
       guildUser.roles.remove(client.spectatorRoleId).then(() => {
         logger.info(`User ${userParameter.username} is now an active player`)
         interaction.reply(`<@${userParameter.id}> is now an active player.`)
       })
       await interaction.reply(`<@${userParameter.id}> is now an active player.`)
-    } else {
-      logger.info(
-        `User ${userParameter.username} not found as an active player or spectator`
-      )
-      await interaction.reply(
-        `Participant <@${userParameter.id}> not found as active player or spectator.`
-      )
     }
   }
 }
